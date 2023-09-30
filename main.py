e4d8ef51473e4599a3322b011e4f16a9a56dda1d
AdrianaRodriguesA <140108574+AdrianaRodriguesA@users.noreply.github.com>
2023-09-30 16:18:07 -0300

    commit1
    30-09-23 16h16

---
+++ main.py
@@ -0,0 +1,78 @@
+import turtle as t
+
+t.setup(540,680, 800, 30)
+t.bgcolor('black')
+t.pensize(3)
+t.pencolor('#306998')
+
+def curve_one():
+    for i in range(90):
+        t.left(1)
+        t.fd(1)
+
+# curve_one()
+
+def curve_two():
+    for i in range(90):
+        t.right(1)
+        t.fd(1)
+
+# curve_two()
+
+def curve_three(d):
+    curve_one()
+    t.fd(d)
+    curve_one()
+
+# curve_three(80)
+
+def halt_logo():
+    t.fd(50)
+    curve_one()
+    t.fd(90)
+    curve_three(80)
+    t.fd(40)
+    t.left(90)
+    t.fd(80)
+    t.right(90)
+    t.fd(10)
+    t.right(90)
+    t.fd(120)
+    curve_three(90)
+    t.fd(30)
+    t.left(90)
+    t.fd(50)
+    curve_two()
+    t.fd(40)
+
+# halt_logo()
+
+def point_pos(x,y):
+    t.penup()
+    t.goto(x,y)
+    t.dot(40, 'black')
+    t.pendown()
+
+# point_pos(100,100)
+
+t.begin_fill()
+t.fillcolor('#306998')
+halt_logo()
+t.end_fill()
+
+t.penup()
+t.goto(20,-10)
+t.pendown()
+t.fillcolor("#ffd43b")
+t.pencolor("#ffd43b")
+t.begin_fill()
+t.setheading(180)
+halt_logo()
+t.end_fill()
+
+point_pos(-50,150)
+point_pos(60,-160)
+t.hideturtle()
+
+
+t.exitonclick()
